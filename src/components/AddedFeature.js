0f89629558a8cd20c6fe71973307a89ea6598c84
Your Name <you@example.com>
2019-08-14 17:07:22 -0400

    got features displayed whenever added

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const AddedFeature = props => {
+  console.log('added_features', props)
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
       <button className="button">X</button>
-      {props.feature.name}
+      {props.feature}
     </li>
   );
 };

672521c385d467eec0c6ad3003ee3359e416f67c
Your Name <you@example.com>
2019-08-14 15:03:50 -0400

    created store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {featureReducer} from './reducers/featureReducer'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-
+export const store = createStore(featureReducer)
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
